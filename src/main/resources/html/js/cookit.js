91b60dbbdce99792d513b415a6e84aac382e774b
Santonian <reinhard.vornholt@gmail.com>
2015-04-30 17:06:25 +0200

    - added bootstrap for the looks - made the first super simple controller

--- src/main/resources/html/js/cookit.js
+++ src/main/resources/html/js/cookit.js
@@ -1,1 +1,12 @@
 var cookitApp = angular.module('cookitApp', [])
+
+cookitApp.controller("ingredientsController", function($scope){
+	$scope.ingredientTypes = [
+	               {name: "Gemüse"}, 
+	               {name: "Gewürz"}, 
+	               {name: "Frucht"}
+	               ];
+	
+	
+	
+});
